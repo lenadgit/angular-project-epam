31a953874f475906de209336cc881836e8a88d94
Andrii Kozakevych <andriikozakevych@gmail.com>
2021-12-01 19:02:09 +0200

    add unit test for delete course method

--- src/app/components/core/page/courselist/courselist.component.spec.ts
+++ src/app/components/core/page/courselist/courselist.component.spec.ts
@@ -1,17 +1,19 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CourselistComponent } from './courselist.component';
 
+beforeEach(async(() => {
+  TestBed.configureTestingModule({
+    declarations: [ CourselistComponent ],
+    schemas: [NO_ERRORS_SCHEMA]
+  })
+  .compileComponents();
+}));
+
 describe('CourselistComponent', () => {
   let component: CourselistComponent;
   let fixture: ComponentFixture<CourselistComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ CourselistComponent ]
-    })
-    .compileComponents();
-  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CourselistComponent);
@@ -22,4 +24,19 @@ describe('CourselistComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('deleteEl', () => {
+    it('should remove object with provided id from array', () => {
+      // Create a mock object for course list
+      const courseListMock = [{id: 1, name: 'test1'}, {id: 2, name: 'test2'}, {id: 3, name: 'test3'}]
+      // Create expected result object
+      const expectedResult = [{id: 2, name: 'test2'}, {id: 3, name: 'test3'}]
+      // Assign our created value to component courses
+      component.courses = [...courseListMock];
+      // Call function with id 1
+      component.deleteEl(1);
+      // Check if function is working correctly
+      expect(component.courses).toEqual(expectedResult);
+    });
+  })
 });
