e26aee6014e1b3314cfd4fd3247d613fe4e15626
Dostupnyilutsk <lena.boruslav@gmail.com>
2021-12-09 22:59:50 +0200

    tests for courselistcomponent

--- src/app/components/core/page/courselist/courselist.component.spec.ts
+++ src/app/components/core/page/courselist/courselist.component.spec.ts
@@ -17,9 +17,16 @@ describe('CourselistComponent', () => {
     fixture = TestBed.createComponent(CourselistComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    expect(compiled.querySelector('h2').textContent).toContain('Video course'); //expect - функція очікування, очікується що у селектора Н2 є тект Video course (такий текст там є насправді)
+  
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(CourselistComponent).toBeTruthy();//очікується що компонент CourselistComponent існує.
   });
+
+  const compiled = fixture.debugElement.nativeElement; // y fixture передається сутність Ангуляра, викликається поле debugElement і присвоюється інстанс nativeElement
+
+  expect(CourselistComponent.button).toEqual('Edit');// очікується що у клмпонента є елемент button значення якого Edit
 });
